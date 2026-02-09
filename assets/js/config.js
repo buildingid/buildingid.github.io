1d4f3153160db451eeefa82d3d70a4f2b7e802cf
buildingid <buildingid@pnnl.gov>
2026-02-09 09:14:27 -0500

    change to prod

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,7 @@
 // Configuration file for Mapbox API credentials
 // IMPORTANT: Do not commit this file to version control
 // Copy config.example.js to config.js and add your own credentials
-
 window.MAPBOX_CONFIG = {
-  token: 'pk.eyJ1IjoiYnVpbGRpbmdpZCIsImEiOiJjbWxmN202emgwMXR3M2Nwc2I5emJ1MjgzIn0.IAc6NSKmtKsI8o47RIW_Yw',
+  token: 'pk.eyJ1IjoiYnVpbGRpbmdpZCIsImEiOiJjbWhudmtobm0wNHRtMmlvcDIyejdsa29rIn0.EiytQQbYQYiILt-22miOng',
   username: 'buildingid'
 };
